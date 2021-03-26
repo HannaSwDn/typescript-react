4943b0296f5611d0bdb66f7a4185ff66b4a59614
Hanna Nyman <hanna.nyman@veridict.com>
2021-03-26 12:44:17 +0100

    Writes Props and markdown for QuestionCard component

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,3 +1,50 @@
 import React from 'react';
 
-const QuestionCard = () => <div>Question Card</div>
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+}
+
+enum Direction {
+  Up = 1,
+  Down,
+  Right,
+  Left
+}
+
+console.log(Direction)
+
+/*
+  to specify to TypeScript that we are using a functional component, we use React.FC
+  we specify the props inside of the angle brackets, <>
+*/
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions
+}) => (
+  <div>
+    <p className="number">
+      Question: { questionNr } / { totalQuestions }
+    </p>
+    <p dangerouslySetInnerHTML = {{ __html: question }}></p>
+    <div>
+      {answers.map(answer => (
+        <div>
+          <button disabled={ userAnswer } onClick = { callback }>
+            <span dangerouslySetInnerHTML = {{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+)
+
+export default QuestionCard;
